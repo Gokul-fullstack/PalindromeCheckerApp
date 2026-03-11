d44db8332c91ecc04c4b6ac4e9c535e01e2f6c23
Gokul-fullstack <gokulr120607@gmail.com>
2026-03-11 10:23:04 +0530

    performance

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,32 +1,32 @@
-public class PalindromeCheckerApp{
+public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
-
-        String input = "level";
-
-        PalindromeService service = new PalindromeService();
-        boolean result = service.checkPalindrome(input);
-
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
-    }
-}
-
-class PalindromeService {
-
-    public boolean checkPalindrome(String input) {
+    public static boolean checkPalindrome(String input) {
 
         int start = 0;
         int end = input.length() - 1;
 
         while(start < end){
-            if(input.charAt(start) != input.charAt(end)){
+            if(input.charAt(start) != input.charAt(end))
                 return false;
-            }
             start++;
             end--;
         }
 
         return true;
+    }
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        long startTime = System.nanoTime();
+
+        boolean result = checkPalindrome(input);
+
+        long endTime = System.nanoTime();
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + (endTime - startTime) + " ns");
     }
 }
